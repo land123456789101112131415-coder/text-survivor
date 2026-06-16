55ccb810533c22e7946f110b400f247800883424
solar132 <130192823+solar132@users.noreply.github.com>
2026-06-16 11:46:12 -0400

    basic health values

---
+++ Main.py
@@ -0,0 +1,40 @@
+brainHealth = 100
+limbs = {
+    "L-arm":{
+        "skin-health":100,
+        "infection-progression":0,
+        "infection-rate":0
+    },
+    "R-arm":{
+        "skin-health":100,
+        "infection-progression":0,
+        "infection-rate":0        
+    },
+    "L-leg":{
+        "skin-health":100,
+        "infection-progression":0,
+        "infection-rate":0
+    },
+    "R-leg":{
+        "skin-health":100,
+        "infection-progression":0,
+        "infection-rate":0        
+    },
+    "torso":{
+        "skin-health":100,
+        "infection-progression":0,
+        "infection-rate":0
+    },
+    "head":{
+        "skin-health":100,
+        "infection-progression":0,
+        "infection-rate":0
+    }
+}
+
+organs = {
+    "heart": {
+        "heart-rate":120,
+        "blood-count":5
+    },
+}
