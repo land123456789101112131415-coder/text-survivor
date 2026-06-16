dfb5e162726edbd3169bc60e2d3293b29a3e9a54
solar132 <130192823+solar132@users.noreply.github.com>
2026-06-16 17:27:57 -0400

    adding a few extra modules

---
+++ modules/stats.py
@@ -0,0 +1,7 @@
+strength = 0
+resiliance = 0
+stamina = 0
+intelligence = 0
+blueprints = []
+
+
---
+++ modules/time.py
@@ -0,0 +1,19 @@
+currentHour = 12
+currentMinute = 0
+currentSecond = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
