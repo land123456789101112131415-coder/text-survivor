68a4d8ca4f245bdc7ec8c1fc662585f5ff1c12eb
solar132 <130192823+solar132@users.noreply.github.com>
2026-06-16 20:57:23 -0400

    Update time.py

--- modules/time.py
+++ modules/time.py
@@ -1,8 +1,11 @@
-currentHour = 12
-currentMinute = 0
-currentSecond = 0
+from datetime import datetime, timedelta
+
+time = "05:00 am"
 
 
+def addTime(time,hour,minute):
+    calcTime = datetime.strptime(time,"%I:%M %p") + timedelta(hours=hour,minutes=minute)
+    return datetime.strftime(calcTime,"%I:%M %p")
 
 
 
